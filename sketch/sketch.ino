f95eddaee3bccd463fef0c89900eb961f956a5f6
Pavel Popchikovsky <ppopchikovsky@gmail.com>
2013-12-19 21:45:18 +0400

    initial commit

---
+++ sketch/sketch.ino
@@ -0,0 +1,76 @@
+// consts
+int LED_PIN_01 = 6;
+int LED_PIN_02 = 9;
+float ledFallSpeed = 200.0f;
+
+// this value constantly changes
+float ledValue = 0.0f;
+
+// time related
+unsigned long prevFrameTime;
+float dt = 0.0;
+
+void setup()
+{
+  // setup LED
+  pinMode(LED_PIN_01, OUTPUT);
+  pinMode(LED_PIN_02, OUTPUT);
+
+  // setup COM
+  Serial.begin(9600);
+
+  // intial value
+  prevFrameTime = millis();
+}
+
+// calcs dt = time in seconds (float) from previous frame
+void calcFrameTime()
+{
+  unsigned long currentFrameTime = millis();
+  prevFrameTime = currentFrameTime - prevFrameTime;
+  if(prevFrameTime < 0)
+    prevFrameTime = currentFrameTime = 0;
+
+  dt = 0.001f * prevFrameTime;
+  prevFrameTime = currentFrameTime;
+}
+
+// reduces LED value over time
+void calcLEDValue()
+{
+  if(ledValue > 0)
+  {
+    ledValue -= ledFallSpeed * dt;
+    if(ledValue < 0)
+      ledValue = 0;
+  }
+}
+
+void updateLEDValue()
+{
+  if(Serial.available() > 0)
+  {
+    float newValue = (float)(Serial.read());
+    if(newValue > ledValue)
+      ledValue = newValue; 
+  }
+}
+
+void setLEDValue()
+{
+  int pinValue = (int)(255.0 * ledValue / 100.0f);
+  analogWrite(LED_PIN_01, pinValue);
+  analogWrite(LED_PIN_02, pinValue);
+}
+
+void loop()
+{
+  updateLEDValue();
+
+  calcFrameTime();
+  calcLEDValue();
+
+  setLEDValue();
+}
+
+
---
+++ vs2010/src/App.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Drawing;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace cs_hdd_status
+{
+    class App : Form
+    {
+        private const int USAGE_CHECK_PERIOD = 100; // ms
+
+        private readonly PerformanceCounter m_diskUsage;
+        private readonly SerialPort m_serialPort;
+        private readonly System.Timers.Timer m_timer;
+
+        private readonly NotifyIcon m_trayIcon;
+        private readonly ContextMenu m_trayMenu;
+        
+        private float m_value;
+
+        private App()
+        {
+            m_diskUsage = new PerformanceCounter("PhysicalDisk", "% Disk Time", "_Total");
+            m_value = 0.0f;
+
+            m_serialPort = new SerialPort("COM3");
+            m_serialPort.Open();
+
+            m_trayMenu = new ContextMenu();
+            m_trayMenu.MenuItems.Add("Exit", OnExit);
+
+            m_trayIcon = new NotifyIcon();
+            m_trayIcon.Text = "HDD Status";
+            m_trayIcon.Icon = new Icon(GetType(), "app.ico");
+            m_trayIcon.ContextMenu = m_trayMenu;
+            m_trayIcon.Visible = true;
+
+            m_timer = new System.Timers.Timer();
+            m_timer.Elapsed += new System.Timers.ElapsedEventHandler(Update);
+            m_timer.Interval = USAGE_CHECK_PERIOD;
+            m_timer.AutoReset = true;
+            m_timer.Start();
+        }
+
+        private void UpdateValue()
+        {
+            // current disk usage value
+            m_value = m_diskUsage.NextValue();
+        }
+
+        private void SendValue()
+        {
+            // send value to serail port
+            m_serialPort.Write(new byte[] { (byte)(m_value) }, 0, 1);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            
+            Visible = false;
+            ShowInTaskbar = false;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                m_serialPort.Close();
+                m_trayIcon.Dispose();
+                m_timer.Stop();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void Update(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            UpdateValue();
+            SendValue();
+        }
+
+        private void OnExit(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        [STAThread]
+        public static void Main()
+        {
+            Application.Run(new App());
+        }
+    }
+}
