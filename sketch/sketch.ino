0a51ebe691ba6a4287cdd99e13bbc2d1ba6450ea
tarpaha <ppopchikovsky@gmail.com>
2014-03-15 16:18:12 +0400

    sketch updated, better fall off

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,7 +1,7 @@
 #define LED_PIN 6
 #define CONTROL_PIN 7
 #define SERIAL_SPEED 9600
-#define SHIFT 9
+#define SHIFT 7
 #define DECREMENT 1
 
 ///////////////////////////////////////////////////////////////
@@ -30,9 +30,15 @@ void updateLED()
 
 void serialEvent()
 {
-    value = Serial.read();
-    digitalWrite(CONTROL_PIN, (value & 1) ? HIGH : LOW);
-    value <<= SHIFT;
+   int new_value = Serial.read();
+   
+   digitalWrite(CONTROL_PIN, (new_value & 1) ? HIGH : LOW);
+   
+   new_value <<= SHIFT;
+   if(new_value > value)
+   {
+       value = new_value;
+   }
 }
 
 void setup()
